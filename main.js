7dc0eb39dee5289ac0ad03e80407ca9c1da71a80
Kononiaka Vladimir <kononiaka.vladimir@gmail.com>
2020-05-25 22:27:48 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+let myCat = {
+  name: "Kitus",
+  species: "cat",
+  favFood: "tuna",
+};
+
+let thePets = [
+  { name: "Kitus", species: "cat", favFood: "tuna" },
+  { name: "Barky", species: "dog", favFood: "carrots" },
+];
+
+thePets.forEach(pet => console.log(pet));
